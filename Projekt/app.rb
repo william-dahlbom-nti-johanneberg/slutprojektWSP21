dd5f4ed2a0a06e18b000ffe581d5082ce8977892
william-dahlbom-nti-johanneberg <60173030+william-dahlbom-nti-johanneberg@users.noreply.github.com>
2021-02-26 14:43:05 +0100

    Update app.rb

--- Projekt/app.rb
+++ Projekt/app.rb
@@ -6,6 +6,32 @@ require 'bcrypt'
 
 enable :sessions 
 
-get('/')
-
+get('/') do 
+ slim(:register )
 end 
+
+
+get('/user') do 
+ slim(:users)
+end 
+
+
+get('/movies') do
+
+end
+
+post('/users/new') do
+    username = params[:username]
+    password = params[:password]
+    password_confirm = params[:password_confirm]
+    
+    if (password == password_confirm)
+      password_digest = BCrypt::Password.create(password)
+      db = SQLite3::Database.new('db/database1.db')
+      db.execute("INSERT INTO users (username,password) VALUES (?,?)",username,password_digest)
+      redirect('/')
+  
+    else
+  #Något om att man inte får ha samma användarnamn som någon annan och att lösenordet stämmer överrens med password_confirm
+    end
+  end
