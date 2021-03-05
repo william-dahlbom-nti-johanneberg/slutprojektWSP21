d5373079b85bb93142429730727210ffd68f1f76
william-dahlbom-nti-johanneberg <60173030+william-dahlbom-nti-johanneberg@users.noreply.github.com>
2021-03-05 13:54:18 +0100

    Update app.rb

--- Projekt/app.rb
+++ Projekt/app.rb
@@ -10,28 +10,67 @@ get('/') do
  slim(:register )
 end 
 
+get('/showlogin') do 
+ slim(:login)
+end
 
-get('/user') do 
- slim(:users)
+post('/login') do 
+  username = params[:username]
+  password = params[:password]
+  db = SQLite3::Database.new('db/database1.db')
+  db.results_as_hash = true
+  result = db.execute("SELECT * FROM users WHERE user_name = ?",username).first
+  pwdigest = result["pwdigest"]
+  id = result["id"]
+  if BCrypt::Password.new(pwdigest) == password
+    session[:id] = result["id"]
+    redirect('/user')
+  else
+    "ICKE MATCHANDE LÖSENORD"
+  end
+end
+
+post('/users/new') do
+  username = params[:username]
+  password = params[:password]
+  password_confirm = params[:password_confirm]
+  
+  if (password == password_confirm)
+    password_digest = BCrypt::Password.create(password)
+    db = SQLite3::Database.new('db/database1.db')
+    db.execute("INSERT INTO users (username,password) VALUES (?,?)",username,password_digest)
+    redirect('/')
+
+  else
+#Något om att man inte får ha samma användarnamn som någon annan och att lösenordet stämmer överrens med password_confirm
+  end
+end
+
+get('/user') do
+  id = session[:id].to_i
+  db = SQLite3::Database.new('db/database1.db')
+  db.results_as_hash = true
+  result = db.execute("SELECT * FROM users WHERE user_id = ?",id)
+  slim(:"users",locals:{users:result})
+end
+
+
+post('/user/movies/new') do 
+
+end 
+
+post('/user/movies/delete') do 
+
+end 
+
+
+post('/user/movies/update') do 
+
 end 
 
 
 get('/movies') do
+  slim(:movies)
+ end
 
-end
 
-post('/users/new') do
-    username = params[:username]
-    password = params[:password]
-    password_confirm = params[:password_confirm]
-    
-    if (password == password_confirm)
-      password_digest = BCrypt::Password.create(password)
-      db = SQLite3::Database.new('db/database1.db')
-      db.execute("INSERT INTO users (username,password) VALUES (?,?)",username,password_digest)
-      redirect('/')
-  
-    else
-  #Något om att man inte får ha samma användarnamn som någon annan och att lösenordet stämmer överrens med password_confirm
-    end
-  end
